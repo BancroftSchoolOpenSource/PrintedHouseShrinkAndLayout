e60c3e9fe3a1c349b86f722dd813060a9ed24ea1
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-23 09:35:00 -0400

    updating

--- ShrinkAndLayout.groovy
+++ ShrinkAndLayout.groovy
@@ -3,6 +3,7 @@ import javax.swing.JFileChooser
 import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
 
 import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
 
 // code here
 
@@ -19,13 +20,15 @@ System.out.println(dir);
 File[] listOfFiles = dir.listFiles();
 
 ArrayList<CSG> stls= []
-
+CSG cutout = new Cylinder(4, 8).toCSG()
 for(int i=0;i<listOfFiles.length;i++) {
 	
 	File file=listOfFiles[i];
 	println "Loading "+file
 	CSG get = Vitamins.get(file)
+	 
 	get=get.scale(scale)
+	get=get.difference(cutout.move(get.getCenter()).toZMin())
 	get.setName(file.getName())
 	stls.add(get)
 }
